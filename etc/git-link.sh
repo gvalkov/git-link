5dadf741ddd82cdaf2bc0a80516e628596a97ebf
Georgi Valkov <georgi.t.valkov@gmail.com>
2012-02-04 02:05:30 +0200

    add zsh and (wip) bash completion

---
+++ etc/git-link.sh
@@ -0,0 +1,16 @@
+
+# bash completion for the the git link command
+# http://github.com/gvalkov/git-link
+
+_git_flow ()
+{
+    local cur prev opts
+
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts="-h --help -v --version -c --clipboard -u --url -b --browser -r --raw"
+	#tbd
+}
+
+# vim: ft=sh:
